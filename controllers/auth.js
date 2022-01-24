2f94a8995f239a7d84dd4673cb892e40beaf29c7
Saliou1920 <diopsaliou45@gmail.com>
2022-01-24 19:24:48 -0400

    adding register and login logic

---
+++ controllers/auth.js
@@ -0,0 +1,36 @@
+const User = require("../models/User");
+const { UnauthenticatedError, BadRequestError } = require("./index");
+const { StatusCodes } = require("http-status-codes");
+
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw new BadRequestError("User already exists");
+  }
+  const newUser = await User.create({ name, email, password });
+  const token = newUser.createToken();
+  res.status(StatusCodes.CREATED).json({ user: { name: newUser.name }, token });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Email and Password are required");
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid email or password");
+  }
+  const isMatch = await user.comparePassword(password);
+  if (!isMatch) {
+    throw new UnauthenticatedError("Invalid email or password");
+  }
+  const token = user.createToken();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+};
+
+module.exports = {
+  register,
+  login,
+};
