62ad466b9f21f44a8522d3a12d7a3ed41a499801
Saliou1920 <diopsaliou45@gmail.com>
2022-01-23 22:34:55 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
+
+const start = async () => {
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
+};
+
+start();
