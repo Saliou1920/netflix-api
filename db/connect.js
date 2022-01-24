c4077db1fd0f592919e94f4ce5714dd4417665a3
Saliou1920 <diopsaliou45@gmail.com>
2022-01-24 01:09:06 -0400

    connecting to the database

--- db/connect.js
+++ db/connect.js
@@ -1,11 +1,6 @@
 const mongoose = require("mongoose");
 
 const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  });
+  return mongoose.connect(url, {});
 };
 module.exports = connectDB;
