13313b05cc2e95d112dc82d20299824494812a31
Saliou1920 <diopsaliou45@gmail.com>
2022-01-24 22:56:37 -0400

    adding timestamps

--- models/User.js
+++ models/User.js
@@ -1,33 +1,40 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: [3, "Name must be at least 3 characters long"],
-    required: [true, "Please add a name"],
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minlength: [3, "Name must be at least 3 characters long"],
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      match: [
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please add a valid email",
+      ],
+      unique: true,
+    },
+    password: {
+      type: String,
+      minlength: [6, "Password must be at least 6 characters long"],
+      required: [true, "Password is required"],
+    },
+    profilePic: {
+      type: String,
+      default:
+        "https://www.kindpng.com/picc/m/21-214439_free-high-quality-person-icon-default-profile-picture.png",
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    match: [
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please add a valid email",
-    ],
-    unique: true,
-  },
-  password: {
-    type: String,
-    minlength: [6, "Password must be at least 6 characters long"],
-    required: [true, "Password is required"],
-  },
-  profilePic: {
-    type: String,
-    default:
-      "https://www.kindpng.com/picc/m/21-214439_free-high-quality-person-icon-default-profile-picture.png",
-  },
-});
+  { timestamps: true }
+);
 
 UserSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
