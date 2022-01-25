c3b80c5f8f261c10826c4887176cbddd8d78779f
Saliou1920 <diopsaliou45@gmail.com>
2022-01-24 22:57:24 -0400

    adding auth logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const { UnauthenticatedError, BadRequestError } = require("./index");
+const { UnauthenticatedError, BadRequestError } = require("../errors/index");
 const { StatusCodes } = require("http-status-codes");
 
 const register = async (req, res) => {
