dee85bc315b92ee3f358f286413a4143070de993
Saliou1920 <diopsaliou45@gmail.com>
2022-01-26 13:38:00 -0400

    bcrypt password

--- models/User.js
+++ models/User.js
@@ -36,9 +36,10 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-UserSchema.pre("save", async function () {
+UserSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 UserSchema.methods.createToken = function () {
